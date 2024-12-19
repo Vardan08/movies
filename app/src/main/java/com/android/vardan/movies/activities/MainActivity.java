4d9152180c7a1cb106ab9004dba736fc01afef9a
Vardan08 <vardan.avetisyan2000@gmail.com>
2024-12-19 23:50:22 +0400

    creating search

--- app/src/main/java/com/android/vardan/movies/activities/MainActivity.java
+++ app/src/main/java/com/android/vardan/movies/activities/MainActivity.java
@@ -2,11 +2,15 @@ package com.android.vardan.movies.activities;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.SearchView;
 
 import com.android.vardan.movies.R;
 import com.android.vardan.movies.data.MovieAdapter;
@@ -14,7 +18,6 @@ import com.android.vardan.movies.model.Movie;
 import com.android.vardan.movies.repository.MovieRepository;
 import com.android.vardan.movies.utils.MovieParser;
 import com.android.volley.VolleyError;
-
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.OnIt
     private RecyclerView recyclerView;
     private ArrayList<Movie> movies;
     private MovieRepository movieRepository;
+    private MovieAdapter movieAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.OnIt
     }
 
     private void setupRecyclerView() {
-        MovieAdapter movieAdapter = new MovieAdapter(MainActivity.this, movies);
+        movieAdapter = new MovieAdapter(MainActivity.this, movies);
         movieAdapter.setOnItemClickListener(this);
         recyclerView.setAdapter(movieAdapter);
     }
@@ -85,6 +89,55 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.OnIt
 
         startActivity(intent);
     }
+
+    // Inflating the menu in the activity's toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        // Get the search item and initialize SearchView
+        MenuItem searchItem = menu.findItem(R.id.actionSearch);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        // Make sure the search view is always expanded and set up query hint
+        searchView.setIconifiedByDefault(false); // Makes the search field visible when the icon is clicked
+        searchView.setQueryHint("Search movies...");
+
+        // When search is expanded, automatically show the keyboard
+        searchView.setFocusable(true);
+        searchView.requestFocusFromTouch();
+
+        // Listener to handle search query text changes
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Optionally trigger an action when search is submitted
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Filter the movies based on the new query
+                movieAdapter.filterMovies(newText);  // Filter movies based on search query
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // Handle search icon click and automatically open the keyboard
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.actionSearch) {
+            // Focus and show the keyboard when the search icon is clicked
+            SearchView searchView = (SearchView) item.getActionView();
+            searchView.requestFocus();
+            searchView.setIconified(false); // Ensure it's expanded and ready for typing
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
-
-
